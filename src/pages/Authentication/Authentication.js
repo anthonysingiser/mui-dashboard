61a7c9de0cbffe38dc7b42e2faaf4f0679ce0fb9
chrysanthemum <asingiser@gmail.com>
2023-05-18 12:32:42 -0400

    create Button component and add to Authentication page

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
+import CommonButton from '../../components/CommonButton/CommonButton'
+import { red } from '@mui/material/colors'
 
 const Authentication = () => {
+    const buttonStyles = {
+        fontSize: 20,
+        fontWeight: 700,
+        backgroundColor: 'red',
+        '&:hover': {
+            backgroundColor: 'yellow'
+        } 
+
+    }
     return (
         <Grid item xs={8}>
             This is Authentication 
+            <CommonButton 
+                variant= 'outlined'
+                size= "large"
+                sx= {buttonStyles}
+            > 
+                Text 
+            </CommonButton>
         </Grid>
     )
 }
