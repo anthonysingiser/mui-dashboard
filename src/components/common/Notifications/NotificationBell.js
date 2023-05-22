750cf2eaf0ed59e1a7375fcaed47b87c142c1acd
chrysanthemum <asingiser@gmail.com>
2023-05-22 12:01:32 -0400

    create NotificationBell component

---
+++ src/components/common/Notifications/NotificationBell.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Badge from '@mui/material/Badge'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+
+const NotificationBell = ( {iconColor, badgeContent} ) => {
+    const newNotifications = `You have ${badgeContent} new notifications` 
+    const noNotifications = 'No new notifications'
+    return (
+        <Tooltip title= {badgeContent ? newNotifications : noNotifications}>
+        <IconButton
+            color={iconColor}
+        >
+        <Badge 
+            badgeContent={badgeContent} 
+            color="error"
+        >
+            <NotificationsIcon />
+        </Badge>
+        </IconButton>
+        </Tooltip>
+    )
+}
+
+export default NotificationBell
--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,51 +1,15 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
-import CommonButton from '../../components/CommonButton/CommonButton'
+import NotificationBell from '../../components/common/Notifications/NotificationBell'
 
 const Authentication = () => {
-    const buttonStyles = {
-        fontSize: '0.875rem',
-        fontWeight: 600,
-        textTransform: 'capitalize',
-        borderRadius: 2.5,
-        '&.MuiButton-contained': {
-            backgroundColor: '#009be5',
-            '&:hover': {
-                backgroundColor: '#006db3',
-            },
-        },
-        '&.MuiButton-outlined': {
-            color: '#fff',
-            borderColor: '#fff',
-            '&:hover': {
-                backgroundColor: 'transparent',
-            },
-        },
-    }
-
     return (
-        <Grid item xs={8}>
-            This is Authentication 
-            <CommonButton 
-                variant= 'contained'
-                size= "large"
-                sx= {buttonStyles}
-            > 
-                Text 
-            </CommonButton>
-            <CommonButton
-                variant='outlined'
-                sx={buttonStyles}
-            >
-                Web setup
-            </CommonButton>
-            <CommonButton
-                variant= 'contained'
-                color={'primary'}
-            >
-                Primary
-            </CommonButton>
-        </Grid>
+    <Grid item xs={8}>
+        <NotificationBell 
+            iconColor='primary'
+            badgeContent={4} 
+        />
+    </Grid>
     )
 }
 
