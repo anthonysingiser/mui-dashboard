2552ce3b0214d37ce44269c5993d53261354fdc5
chrysanthemum <asingiser@gmail.com>
2023-05-23 16:30:31 -0400

    link functionality between menu and notification

--- src/components/common/Notifications/NotificationBell.js
+++ src/components/common/Notifications/NotificationBell.js
@@ -3,23 +3,44 @@ import Badge from '@mui/material/Badge'
 import NotificationsIcon from '@mui/icons-material/Notifications'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
+import BasicMenu from '../BasicMenu/BasicMenu'
 
-const NotificationBell = ( {iconColor, badgeContent} ) => {
+const NotificationBell = ( { iconColor, badgeContent } ) => {
     const newNotifications = `You have ${badgeContent} new notifications` 
     const noNotifications = 'No new notifications'
+
+    const [open, setOpen] = React.useState(false)
+    const [anchorEl, setAnchorEl] = React.useState(null)
+
+    const handleOpen = (event) => {
+        setAnchorEl(event.currentTarget)
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+    }
     return (
-        <Tooltip title= {badgeContent ? newNotifications : noNotifications}>
-        <IconButton
-            color={iconColor}
-        >
-        <Badge 
-            badgeContent={badgeContent} 
-            color="error"
-        >
-            <NotificationsIcon />
-        </Badge>
-        </IconButton>
-        </Tooltip>
+        <div>
+            <Tooltip title= {badgeContent ? newNotifications : noNotifications}>
+                <IconButton
+                    color= {iconColor}
+                    onClick = {handleOpen}
+                    anchorEl= {anchorEl}
+                >
+                <Badge 
+                    badgeContent={badgeContent} 
+                    color="error"
+                >
+                    <NotificationsIcon />
+                </Badge>
+                </IconButton>
+            </Tooltip>
+        <BasicMenu 
+            open= {open}
+            anchorEl= {anchorEl}
+            handleClose = {handleClose}
+        />
+        </div>
     )
 }
 
--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
 import NotificationBell from '../../components/common/Notifications/NotificationBell'
-import BasicMenu from '../../components/common/BasicMenu/BasicMenu'
+
 
 const Authentication = () => {
     return (
     <Grid item xs={8}>
+        This is Authentication
         <NotificationBell 
             iconColor='primary'
             badgeContent={0} 
         />
-        <BasicMenu/>
     </Grid>
     )
 }
