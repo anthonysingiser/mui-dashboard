59e7a613834d4411f83ad0a34c06d784b3822fd8
chrysanthemum <asingiser@gmail.com>
2023-05-23 12:20:13 -0400

    add reusable menu component

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
 import NotificationBell from '../../components/common/Notifications/NotificationBell'
+import BasicMenu from '../../components/common/BasicMenu/BasicMenu'
 
 const Authentication = () => {
     return (
     <Grid item xs={8}>
         <NotificationBell 
             iconColor='primary'
-            badgeContent={4} 
+            badgeContent={0} 
         />
+        <BasicMenu/>
     </Grid>
     )
 }
