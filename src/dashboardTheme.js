c8d96d1f5572db52f9270d1d8984fa11ec39bcb2
chrysanthemum <asingiser@gmail.com>
2023-05-28 11:31:40 -0400

    style Header component

--- src/dashboardTheme.js
+++ src/dashboardTheme.js
@@ -2,38 +2,50 @@ import {createTheme} from '@mui/material/styles'
 
 export const dashboardTheme = createTheme ({
     components: {
-
         MuiButton: {
-
             styleOverrides: {
-
-                 contained: {
-            
-
-                 },
+                root: {
+                    fontSize: '0.875rem',
+                    fontWeight: 600,
+                    borderRadius: 8.5,
+                    textTransform: 'none',
+                    '&.MuiButton-contained': {
+                        backgroundColor: '#009be5',
+                        '&:hover': {
+                            backgroundColor: '#006db3'
+                        },
+                    },
+                    '&.MuiButton-outlined':{
+                        color:"#fff",
+                        borderColor: 'rgba(255, 255, 255, 0.7)',
+                        '&:hover': {
+                            backgroundColor: 'rgba(0, 0, 0, 0.04)'
+                        },
+                    },
+                },
+            },
+        },
+        MuiSvgIcon: {
+            styleOverrids: {
+                root: {
+                    fontSize: '1.7rem',
+                },
             },
         },
     },
     palette: {
-        primary: {
-            main: '#0044ff'
+        white: {
+            main: '#fff',
         }
     },
     typography: {
-        fontFamily: [
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-        ].join(','),
-      },
-      spacing: [4, 8, 12, 16, 20]
+        h1: {
+            fontSize: '1.6rem',
+            fontWeight: 600,
+            color: '#fff',
+            letterSpacing: '0.5px'
+        },
+    },
 })
 
 export default dashboardTheme
