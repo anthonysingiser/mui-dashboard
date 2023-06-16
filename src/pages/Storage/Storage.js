028d63a42a3ae2e65d50f323e478cf0b0044273d
chrysanthemum <asingiser@gmail.com>
2023-06-16 11:19:54 -0400

    create BasicSnackbar and add to Storage page

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
+import React, { useState } from 'react'
+import GridWrapper from '../../components/common/GridWrapper/GridWrapper'
+import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar'
+import CommonButton from '../../components/common/CommonButton/CommonButton'
 
 const Storage = () => {
+    const [open, setOpen] = useState(false);
+  
+    const onClick = () => {
+      setOpen(true);
+    };
+  
+    const onClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setOpen(false);
+    };
+
+    const message = "This is a success message!"
+
     return (
-        <Grid item xs={8}>
-            This is Storage 
-        </Grid>
+        <GridWrapper>
+        <CommonButton 
+            variant="contained" 
+            onClick={onClick}
+        >
+          Open success snackbar
+        </CommonButton> 
+            <BasicSnackbar
+                open={open}
+                severity="success"
+                onClose={onClose}
+                message={message}
+            />
+        </GridWrapper>
     )
 }
 
